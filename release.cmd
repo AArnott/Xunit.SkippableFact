c9b73c4466d18d452abeaf11988e246194410ae2
Andrew Arnott <andarno@microsoft.com>
2015-07-19 22:16:05 -0700

    Fix up release script

--- release.cmd
+++ release.cmd
@@ -2,12 +2,12 @@
 
 git clean -fdx src
 nuget restore src
-msbuild /nologo /v:minimal /m /fl src\Xunit.Combinatorial.sln /p:configuration=release,UseNonZeroBuildNumber=true
-vstest.console.exe src\Xunit.Combinatorial.Tests\bin\release\Xunit.Combinatorial.Tests.dll /TestAdapterPath:packages
+msbuild /nologo /v:minimal /m /fl src\Xunit.SkippableFact.sln /p:configuration=release,UseNonZeroBuildNumber=true
+vstest.console.exe src\Xunit.SkippableFact.Tests\bin\release\Xunit.SkippableFact.Tests.dll /TestAdapterPath:packages
 
 @echo Last steps to release:
 @echo git tag v1.0.******
 @echo git push origin v1.0.******
-@echo nuget push src\Xunit.Combinatorial.NuGet\bin\release\Xunit.Combinatorial.*.nupkg
+@echo nuget push src\Xunit.SkippableFact.NuGet\bin\release\Xunit.SkippableFact.*.nupkg
 
 @popd
