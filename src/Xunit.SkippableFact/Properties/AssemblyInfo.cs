4d47d6439b6a4f18d7f5f58db8b9dbef43f2f4be
Andrew Arnott <andarno@microsoft.com>
2015-11-22 20:26:53 -0800

    Remove unnecessary attributes and extra version file

--- src/Xunit.SkippableFact/Properties/AssemblyInfo.cs
+++ src/Xunit.SkippableFact/Properties/AssemblyInfo.cs
@@ -4,15 +4,7 @@
 using System.Reflection;
 using System.Resources;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Xunit.SkippableFact")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Xunit.SkippableFact")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
