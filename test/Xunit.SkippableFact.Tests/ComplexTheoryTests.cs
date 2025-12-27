5a678e789fd363fce99b71597ae5bdd01666e118
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2025-12-27 14:01:09 +0100

    Make linter happy

--- test/Xunit.SkippableFact.Tests/ComplexTheoryTests.cs
+++ test/Xunit.SkippableFact.Tests/ComplexTheoryTests.cs
@@ -1,12 +1,10 @@
-﻿namespace Xunit.SkippableFact.Tests;
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Microsoft Public License (Ms-PL). See LICENSE.txt file in the project root for full license information.
+
+namespace Xunit.SkippableFact.Tests;
 
 public class ComplexTheoryTests
 {
-    public struct ValueType(ushort value)
-    {
-        public ushort Value { get; set; } = value;
-    }
-
     public static IEnumerable<object[]> Packets =>
     [
         [new ValueType(0x0001)],
@@ -19,4 +17,6 @@ public class ComplexTheoryTests
     {
         Skip.If(packetId.Value == 0x0001, "Test packet ID to skip.");
     }
+
+    public record struct ValueType(ushort Value);
 }
