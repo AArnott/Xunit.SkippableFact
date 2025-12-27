fd69bb10af3e50e2cf4d0488744d5f2d51fc782d
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2025-12-27 18:31:20 +0100

    Fix wrong failed state for rider (#105)
    * Fix wrong failed state for rider
    
    * Make linter happy

---
+++ test/Xunit.SkippableFact.Tests/ComplexTheoryTests.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Microsoft Public License (Ms-PL). See LICENSE.txt file in the project root for full license information.
+
+namespace Xunit.SkippableFact.Tests;
+
+public class ComplexTheoryTests
+{
+    public static IEnumerable<object[]> Packets =>
+    [
+        [new ValueType(0x0001)],
+        [new ValueType(0x0002)]
+    ];
+
+    [SkippableTheory]
+    [MemberData(nameof(Packets))]
+    public void SkipsCorrectly(ValueType packetId)
+    {
+        Skip.If(packetId.Value == 0x0001, "Test packet ID to skip.");
+    }
+
+    public record struct ValueType(ushort Value);
+}
